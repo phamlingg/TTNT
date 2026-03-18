41fb36aa060ee454ebb074a7f77b3824ecb9000b
phamlingg <pham204linh@gmail.com>
2026-03-18 19:40:25 +0700

    Add files via upload

---
+++ btl_ttnt/AddDatatoDatabase.py
@@ -0,0 +1,56 @@
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import db
+
+cred = credentials.Certificate("serviceAccountKey.json")
+firebase_admin.initialize_app(cred, {
+    'databaseURL': "https://btl-ttnt-8798a-default-rtdb.asia-southeast1.firebasedatabase.app/"
+})
+
+ref = db.reference('Students')
+
+data = {
+
+    "123456": {
+        "name": "Luong Ngoc Nam",
+        "major": "Computer Engineering",
+        "starting_year": 2022,
+        "total_attendance": 10,
+        "standing": "A",
+        "year": 4,
+        "last_attendance_time": "2026-02-23 00:00:00"
+        },
+    "321654":
+        {
+            "name": "Murtaza Hassan",
+            "major": "Robotics",
+            "starting_year": 2017,
+            "total_attendance": 7,
+            "standing": "G",
+            "year": 4,
+            "last_attendance_time": "2022-12-11 00:54:34"
+        },
+    "852741":
+        {
+            "name": "Emly Blunt",
+            "major": "Economics",
+            "starting_year": 2021,
+            "total_attendance": 12,
+            "standing": "B",
+            "year": 1,
+            "last_attendance_time": "2022-12-11 00:54:34"
+        },
+    "963852":
+        {
+            "name": "Elon Musk",
+            "major": "Physics",
+            "starting_year": 2020,
+            "total_attendance": 7,
+            "standing": "G",
+            "year": 2,
+            "last_attendance_time": "2022-12-11 00:54:34"
+        }
+}
+
+for key, value in data.items():
+    ref.child(key).set(value)
